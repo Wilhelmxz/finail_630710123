f0e20063a97f929ba3e8800a0f5b93d8ad405540
Nuzmee <chinjicup@gmail.com>
2023-03-19 12:24:41 +0700

    Update

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../models/poll.dart';
+import '../../services/api.dart';
 import '../my_scaffold.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,15 +14,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Poll>? _polls;
   var _isLoading = false;
+  bool _isError = false;
+  String _errMessage = '';
 
   @override
   void initState() {
     super.initState();
-    _loadData();
+    loadData();
   }
 
-  _loadData() async {
-    // todo: Load list of polls here
+  loadData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await Future.delayed(const Duration(seconds: 3), () {});
+
+    try {
+      var result = await ApiClient().getAllPolls();
+      setState(() {
+        _polls = result;
+      });
+    } catch (e) {
+      setState(() {
+        _errMessage = e.toString();
+        _isError = true;
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
@@ -47,8 +70,20 @@ class _HomePageState extends State<HomePage> {
     return ListView.builder(
       itemCount: _polls!.length,
       itemBuilder: (BuildContext context, int index) {
-        // todo: Create your poll item by replacing this Container()
-        return Container();
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('${_polls![index].id} . ${_polls![index].question}'),
+              Column(crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  OutlinedButton(onPressed: () {  },
+                  child: Text(_polls![index].choices.join("\n "))),
+                ],
+              ),
+            ],
+          ),
+        );
       },
     );
   }
